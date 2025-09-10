a0c4acf0b50aed14073309ab0b565aee643a58e6
Mushfiqur Rahman Siddiqui <mushfiq.rahat39@gmail.com>
2025-09-10 21:14:35 +0200

    Update comments

--- all_blocks.py
+++ all_blocks.py
@@ -1,8 +1,10 @@
 from block import Block
 from position import Position
 
+# L-shaped Tetris block
 class LBlock(Block):
     def __init__(self):
+        # Define the block in all 4 states(0, 90, 180, 270 degrees)
         super().__init__(id=1)
         self.cells= {
             0: [Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)],
@@ -11,11 +13,14 @@ class LBlock(Block):
             3: [Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 1)]
 
         }
-        self.move(0, 3)  # Adjust the initial position of the block
+        # Move block to its initial spawn position
+        self.move(0, 3)  
 
+# J-shaped block
 class JBlock(Block):
     def __init__(self):
         super().__init__(id=2)
+        # Rotation of J-block
         self.cells = {
             0: [Position(0, 0), Position(1, 0), Position(1, 1), Position(1, 2)],
             1: [Position(0, 1), Position(0, 2), Position(1, 1), Position(2, 1)],
@@ -23,9 +28,12 @@ class JBlock(Block):
             3: [Position(0, 1), Position(1, 1), Position(2, 0), Position(2, 1)]
         }
         self.move(0,3)
+
+# I-shaped block
 class IBlock(Block):
     def __init__(self):
         super().__init__(id=3)
+        # Rotation of I-block
         self.cells = {
             0: [Position(1, 0), Position(1, 1), Position(1, 2), Position(1, 3)],
             1: [Position(0, 2), Position(1, 2), Position(2, 2), Position(3, 2)],
@@ -34,9 +42,11 @@ class IBlock(Block):
         }
         self.move(-1,3)
 
+# O-shaped block
 class OBlock(Block):
     def __init__(self):
         super().__init__(id=4)
+        # O-block looks the same in all rotations
         self.cells = {
             0: [Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)],
             1: [Position(0, 0), Position(0, 1), Position(1, 0), Position(1, 1)],
@@ -45,9 +55,11 @@ class OBlock(Block):
         }
         self.move(0,4)
 
+# S-shaped block
 class SBlock(Block):
     def __init__(self):
         super().__init__(id=5)
+        # Rotation of S-block
         self.cells = {
             0: [Position(0, 1), Position(0, 2), Position(1, 0), Position(1, 1)],
             1: [Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 2)],
@@ -56,9 +68,11 @@ class SBlock(Block):
         }
         self.move(0,3)
 
+# T-shaped block
 class TBlock(Block):
     def __init__(self):
         super().__init__(id=6)
+        # Rotation of T-block
         self.cells = {
             0: [Position(0, 1), Position(1, 0), Position(1, 1), Position(1, 2)],
             1: [Position(0, 1), Position(1, 1), Position(1, 2), Position(2, 1)],
@@ -67,9 +81,11 @@ class TBlock(Block):
         }
         self.move(0,3)
 
+# Z-shaped block
 class ZBlock(Block):
     def __init__(self):
         super().__init__(id=7)
+        # Rotation of Z-block
         self.cells = {
             0: [Position(0, 0), Position(0, 1), Position(1, 1), Position(1, 2)],
             1: [Position(0, 2), Position(1, 1), Position(1, 2), Position(2, 1)],
