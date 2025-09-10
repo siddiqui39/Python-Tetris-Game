091d1ef3dad54038a752f45b69c547666e8f97ee
Mushfiqur Rahman Siddiqui <mushfiq.rahat39@gmail.com>
2025-09-10 22:03:38 +0200

    comments

--- mycolors.py
+++ mycolors.py
@@ -1,5 +1,9 @@
 class MyColors:
     
+    """
+    Defines color constants (RGB tuples) used throughout the Tetris game.
+    Provides a helper method to map block IDs to their colors
+    """
     dark_grey = (26, 31, 40)
     green= (47, 230, 23)
     red= (232, 18, 18)
@@ -14,4 +18,9 @@ class MyColors:
 
     @classmethod
     def get_cell_colors(cls):
+    """
+    Return a list of block colors indexed by block ID.
+    Index 0 = background (dark grey)
+    IDs 1-7 = different Tetrominoes.
+    """
         return [cls.dark_grey, cls.green, cls.red, cls.orange, cls.yellow, cls.purple, cls.cyan, cls.blue]
